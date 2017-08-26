3dbb1e4ca2de3de71758025a5abf00e59a3e6f35
S Shashank <s.shashank2401@gmail.com>
2017-08-26 23:52:34 +0530

    Added printPrompt.c

--- read.c
+++ read.c
@@ -1,31 +1,29 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 char inp_string[1024];
 char ** array_tokens;
 
-int main()
-{
+int main() {
 	scanf("%1024[^\n]",inp_string);
     array_tokens = malloc(1024 * sizeof(char*));
 	
-	char s[2] = " \t";
+	char delim[2] = " \t";
 	
 	char *token;
-	   
+
 	/* get the first token */
-	token = strtok(inp_string, s);
-	int size=0,i;   
+	token = strtok(inp_string, delim);
+
+	int size = 0, i;
+	
 	/* walk through other tokens */
-	while(token != NULL ) 
-	{
+	while (token != NULL ) {
 	    array_tokens[size++]=token;
 	    token = strtok(NULL, s);
 	}
 
-	for(i=0;i<size;i++)
-	{
+	for (i = 0; i < size; i++) {
 		printf("%s\n",array_tokens[i]);
 	}
-
 }
