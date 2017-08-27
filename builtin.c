c871a69eb8cd3071c2679d9d58bcf2f08610d13e
S Shashank <s.shashank2401@gmail.com>
2017-08-27 15:14:44 +0530

    Updated builtin.c

--- builtin.c
+++ builtin.c
@@ -1,3 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
-int 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char *lsh_pwd() {
+	if (getcwd(home_dir, sizeof(home_dir)) != NULL) {
+		return home_dir;
+	}
+	else {
+		perror("getcwd() error");
+	}
+	return "";
+}
+
+int shell_cd(char *args[]) {
+	if (args[1] == NULL || strcmp(args[1], "~") == 0) {
+		if (chdir(home_dir) != 0) {
+			perror("Couldn't run cd command");
+		}
+	}
+	else {
+		char cd_dir[MAXN];
+		int k = 0;
+		for (int j = 0; j < strlen(args[1]); j++) {
+			if (j == 0 && args[1][j] == '~') {
+				strcpy(cd_dir, home_dir);
+				k = strlen(home_dir);
+			}
+			else {
+				cd_dir[k++] = args[1][j];
+			}
+		}
+		cd_dir[k++] = '\0';
+		printf("%s\n", cd_dir);
+		if (chdir(cd_dir) != 0) {
+			perror("Couldn't run cd command");
+		}
+	}
+	return 1;
+}
+
+int shell_echo(char * args[]) {
+	
+	return 1;
+}
+
+int shell_exit() {
+	return 0;
+}
