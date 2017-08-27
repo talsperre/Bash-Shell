8f7b21dc44c1fa44c068efb144d8d31e3aa07038
S Shashank <s.shashank2401@gmail.com>
2017-08-28 00:02:06 +0530

    Update echo.c

--- Experiment/echo.c
+++ Experiment/echo.c
@@ -3,32 +3,21 @@
 #include <string.h>
 #include <unistd.h>
 
-int shell_echo(char * args[]) 
-{
-	if (args[1]==NULL)
-	{
+int shell_echo (char * args[]) {
+	if (args[1]==NULL) {
 		printf("\n");
 		return 1;
 	}
-	else
-	{
-		int i=1;
-		for (i = 1; args[i] != ""; i++) {
+	else {
+		int i;
+		for (i = 1; args[i] != NULL; i++) {
 			printf("%s ", args[i]);
 		}
-		while(args[i]!="")
-		{
-			write(1, args[i], strlen(args[i]));
-			write(1," ",1);
-			i++;
-		}
-		write(1,"\n",1);
-
 	}
-
+	return 1;
 }
-int main()
-{
-	char * arr[]={"echo","a","b","c","d",""};
+
+int main() {
+	char * arr[]={"echo","a","b","c","d", NULL};
 	shell_echo(arr);
 }
